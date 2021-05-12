685e8feb01d1c3166ecb2943d233743b132cb5e1
wbxing <wenbiaoxing@gmail.com>
2021-05-12 15:44:09 +0800

    test

--- test.go
+++ test.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello World")
+    // fmt.Println("Hello World")
+    // fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京")
+    
+    // 变量
+    var i int
+    // i = 10
+    fmt.Println("i = ", i)
 }
-
-
